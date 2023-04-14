1b4a4642f0b6a8edfeb2231c0703cc41d68e1844
Quang Nguyen The <quangnguyenthe@Quangs-MacBook-Air.local>
2023-04-14 14:29:13 +0700

    display items on homepage

--- src/server/api/routers/listings.ts
+++ src/server/api/routers/listings.ts
@@ -3,10 +3,14 @@ import { z } from "zod";
 import {
   createTRPCRouter,
   protectedProcedure,
+  publicProcedure,
 } from "~/server/api/trpc";
 
 export const listingsRouter = createTRPCRouter({
-  create: protectedProcedure //use the protected procedure to create a mutation. This procedure has a middleware that checks if user is logged in
+  list: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.listing.findMany();
+  }),
+  create: protectedProcedure
     .input(
       z.object({ name: z.string(), description: z.string(), price: z.number() })
     )
