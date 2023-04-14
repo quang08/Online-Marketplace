19ccd97537949d3c5373c723e3505ae5254b7620
Quang Nguyen The <quangnguyenthe@Quangs-MacBook-Air.local>
2023-04-14 08:56:12 +0700

    refactor

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,6 +29,8 @@ const config = {
       },
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-misused-promises" : "off" ,
+    "@typescript-eslint/no-floating-promises": "off",
   },
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import { useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
 
